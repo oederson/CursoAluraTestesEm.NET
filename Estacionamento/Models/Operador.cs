acec883728294833a62854d5c7b4a29686e93235
oederson <eder.hu@live.com.pt>
2023-10-05 02:37:02 -0300

    Primeiro commit

---
+++ Estacionamento/Models/Operador.cs
@@ -0,0 +1,24 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Estacionamento.Models
+{
+    public class Operador
+    {
+        // Campos
+        private string _matricula;
+        private string _nome;
+
+        //Propriedades
+        public string Matricula { get => _matricula;  private set => _matricula = value; }
+        public string Nome { get => _nome; set => _nome = value; }
+
+        //Construtor
+        public Operador()
+        {
+            this.Matricula = new Guid().ToString().Substring(0,8);
+        }
+    }
+}
---
+++ Estacionamento/Models/TipoVeiculo.cs
@@ -0,0 +1,14 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Estacionamento.Models
+{
+       //Enumeração para classificar os veículos em carros ou motos.
+    public enum TipoVeiculo
+    {
+        Automovel=0,
+        Motocicleta=1
+    }
+}
